b687af25c963459d73c153869f9c1d44c74cb5b7
life-in-devops <lifeindevops@gmail.com>
2025-01-26 14:52:40 +0530

    updated eks file to support IMDSv2

--- eks.tf
+++ eks.tf
@@ -55,7 +55,7 @@ resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
 resource "aws_eks_cluster" "eks_cluster" {
   name     = var.cluster-name
   role_arn = aws_iam_role.eks_role.arn
-  version  = "1.27"
+  version  = "1.30"
 
   vpc_config {
     subnet_ids = [
@@ -71,6 +71,17 @@ resource "aws_eks_cluster" "eks_cluster" {
   depends_on = [aws_iam_role_policy_attachment.AmazonEKSClusterPolicy]
 }
 
+resource "aws_launch_template" "eks_nodes_template" {
+  name = "eks-nodes-launch-template"
+
+  instance_type = "t3.small"
+
+  metadata_options {
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 1
+    http_endpoint               = "enabled"
+  }
+}
 
 resource "aws_eks_node_group" "eks_nodes" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
@@ -82,23 +93,25 @@ resource "aws_eks_node_group" "eks_nodes" {
     aws_subnet.private-ap-south-1b.id
   ]
 
-  capacity_type  = "SPOT"
-  instance_types = ["t3.small"]
-
+  capacity_type = "SPOT"
   scaling_config {
-    desired_size = 3
-    max_size     = 5
+    desired_size = 2
+    max_size     = 3
     min_size     = 1
   }
 
-  update_config {
-    max_unavailable = 1
+  launch_template {
+    id      = aws_launch_template.eks_nodes_template.id
+    version = "$Latest"
   }
 
   labels = {
     role = "general"
   }
 
+  update_config {
+    max_unavailable = 1
+  }
 
   depends_on = [
     aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
@@ -106,3 +119,44 @@ resource "aws_eks_node_group" "eks_nodes" {
     aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
   ]
 }
+
+# resource "aws_eks_node_group" "eks_nodes" {
+#   cluster_name    = aws_eks_cluster.eks_cluster.name
+#   node_group_name = "eks_nodes"
+#   node_role_arn   = aws_iam_role.eks_nodes.arn
+
+#   subnet_ids = [
+#     aws_subnet.private-ap-south-1a.id,
+#     aws_subnet.private-ap-south-1b.id
+#   ]
+
+#   capacity_type  = "SPOT"
+#   instance_types = ["t3.small"]
+
+#   scaling_config {
+#     desired_size = 2
+#     max_size     = 3
+#     min_size     = 1
+#   }
+
+#   # metadata_options {
+#   #   http_tokens          = "opctional"  # Enforces IMDSv2
+#   #   http_put_response_hop_limit = 1   # Limits network hops
+#   #   http_endpoint         = "enabled" # Enables metadata service
+#   # }
+
+#   update_config {
+#     max_unavailable = 1
+#   }
+
+#   labels = {
+#     role = "general"
+#   }
+
+
+#   depends_on = [
+#     aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+#     aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+#     aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+#   ]
+# }
